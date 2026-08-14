f59cfaacb0997996a29ad72c11e2c5ffb193052c
silentgoodbye <silentgoodbye@silentgoodbye.home>
2026-08-14 11:53:01 -0700

    site key

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 window.RF_API = (location.hostname.indexOf('github.io') > -1) ? 'https://revenueforge-api.onrender.com' : '';
-window.RF_RECAPTCHA_SITE = "PASTE_SITE_KEY_HERE";
+window.RF_RECAPTCHA_SITE = "6LeeFIYtAAAAALrKH0iC4xqRygsHTZWI70Egnw86";
